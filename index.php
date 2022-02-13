c9218f535430bbfc94a65e56538c5939b6f8dd16
frame88 <fleuzzi97@gmail.com>
2022-02-13 18:31:53 +0100

    traccia

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//  Dopo aver creato un nuovo database nel vostro phpMyAdmin e aver importato lo schema allegato,
+//  eseguite le query del file allegato.
+//  Dopo aver testato le vostre query con phpMyAdmin, riportatele in un file txt
+//  e caricatelo nella vostra repo.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>db-uni</title>
+</head>
+<body>
+    
+</body>
+</html>
